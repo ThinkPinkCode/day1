a68f97bc09d06b03e0e32a6e647a5935d36e1e7c
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-27 12:56:26 -0500

    added remove transition

--- scripts.js
+++ scripts.js
@@ -3,19 +3,25 @@
  */
 
 
-window.addEventListener('keydown', function(e){
+window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
+    if (!audio) return;
     audio.currentTime = 0;
     audio.play();
 
     key.classList.add('playing');
-
-    //setTimeout(key.classList.remove('playing'), 5000);
-
-
-
 });
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+
+    if (e.propertyName !== 'transform')
+        return;
+
+    this.classList.remove('playing');
+}
 
 
