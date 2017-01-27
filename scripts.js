13dcbf0f34717fa09440728f3aea4f72f03dd5d6
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-27 10:15:31 -0500

    updated sounds

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 /**
  * Created by melanie.myers on 1/23/17.
  */
+
+
+
+function nowPlaying(elmnt){
+
+    console.log("clicked");
+    console.log(elmnt['data-key']);
+    elmnt.className = "playing";
+
+}
+
+const audio =  document.querySelector(`audio`)
