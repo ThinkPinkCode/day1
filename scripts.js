5f53c0ae0b127da78e361b7ce0547ba44233c881
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-27 10:52:59 -0500

    corrected wav files

--- scripts.js
+++ scripts.js
@@ -3,13 +3,15 @@
  */
 
 
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
 
-function nowPlaying(elmnt){
+    
+});
 
-    console.log("clicked");
-    console.log(elmnt['data-key']);
-    elmnt.className = "playing";
 
-}
-
-const audio =  document.querySelector(`audio`)
