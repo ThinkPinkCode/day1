0325ed443e2f3df7d65ceb3b7c74be6e9194de9a
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-27 18:52:21 -0500

    added custom sound

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@
 
 window.addEventListener('keydown', playSound);
 
+
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
